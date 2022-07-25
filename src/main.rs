bc0f2bc4a53dd41c81f1fcf3d42a1ab1ee19ef20
shaka <shmurakami1104@gmail.com>
2022-07-25 23:24:22 +0900

    parse timestamp to date time with timezone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::{Local, TimeZone, Timelike};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Timestamp to show as system date time
+    #[clap(short, value_parser)]
+    timestamp: i64,
+    /// Do not show date and timezone.
+    #[clap(short, long, value_parser, default_value_t = false)]
+    only_time: bool,
+}
+
+fn main() -> Result<(), String> {
+    let args = Cli::parse();
+    let t: i64 = args.timestamp;
+    let only = args.only_time;
+
+    let l = Local.timestamp(t, 0);
+
+    if only {
+        println!("{:02?}:{:02?}:{:02?}", l.hour(), l.minute(), l.second());
+    } else {
+        println!("{}", l.to_string());
+    }
+
+    Ok(())
 }
